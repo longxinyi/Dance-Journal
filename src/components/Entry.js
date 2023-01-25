c86f9d7260780560d9e52e60c439ac790eebdbe5
Long Xin Yi <long.xinyi00@gmail.com>
2023-01-25 13:23:23 +0800

    first commit

---
+++ src/components/Entry.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Entry = (props) => {
+  return (
+    <React.Fragment>
+      <header>Dance video</header>
+      <h2>date</h2>
+      <input></input>
+    </React.Fragment>
+  );
+};
+
+export default Entry;
